9255028358faf1911b17421ba0945db0079d1f0d
Alexandra-Luiza Popov <proallup@gmail.com>
2022-08-02 13:05:01 +0300

    Adaptare comparare, genul.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let bmi1;
+let bmi2;
 
 document.querySelector('.verify1').addEventListener('click', function() {
 
   const inaltime1 = document.querySelector('.inputInaltime1').value;
   const greutate1 = document.querySelector('.inputGreutate1').value;
 
-  let bmi1 = greutate1 / inaltime1;
+  bmi1 = greutate1 / (inaltime1**2);
 
   document.querySelector('.clasaBMI1').textContent = (`BMI-ul tău este ${bmi1}.`);
 });
+
 
 document.querySelector('.verifyAge1').addEventListener('click', function() {
 
@@ -19,17 +21,30 @@ document.querySelector('.verifyAge1').addEventListener('click', function() {
   document.querySelector('.clasaVarsta1').textContent = (`Vârsta ta este ${varsta1}.`);
 });
 
-let bmi2;
-
 document.querySelector('.verify2').addEventListener('click', function() {
 
   const inaltime2 = document.querySelector('.inputInaltime2').value;
   const greutate2 = document.querySelector('.inputGreutate2').value;
 
-  let bmi2 = greutate2 / inaltime2;
+  bmi2 = greutate2 / (inaltime2**2);
 
   document.querySelector('.clasaBMI2').textContent = (`BMI-ul tău este ${bmi2}.`);
 });
+
+
+
+document.querySelector('.compara').addEventListener('click', function() {
+  if (bmi1 > bmi2) {
+    document.querySelector('.clasaCompara').textContent = (`BMI persoană I > BMI persoană II.`)
+  }
+  else if (bmi2 > bmi1) {
+    document.querySelector('.clasaCompara').textContent = (`BMI persoană II > BMI persoană I.`) 
+  }
+  else {
+    document.querySelector('.clasaCompara').textContent = (`Introduceți toate datele.`)
+  }
+});
+
 
 document.querySelector('.verifyAge2').addEventListener('click', function() {
 
@@ -38,14 +53,4 @@ document.querySelector('.verifyAge2').addEventListener('click', function() {
   let varsta2 = new Date().getFullYear() - anulNasterii2;
 
   document.querySelector('.clasaVarsta2').textContent = (`Vârsta este ${varsta2}.`);
-});
-
-
-document.querySelector('.compara').addEventListener('click', function() {
-  if (bmi1 > bmi2) {
-    document.querySelector('.clasaCompara').textContent = (`BMI persoană I > BMI persoană II.`)
-  }
-  else {
-    document.querySelector('.clasaCompara').textContent = (`BMI persoană II > BMI persoană I.`)
-  }
 });
